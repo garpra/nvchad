fc57d5d651f2ed70a3f15c350e1a8cca25e2bc7c
Gilang Arya Pradana <gilangarya272001@gmail.com>
2026-04-24 21:44:02 +0700

    add colorizer, spectre, wakatime, and configurate blink

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -20,6 +20,17 @@ return {
   },
 
   { import = "nvchad.blink.lazyspec" },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      enabled = function()
+        local ok, result = pcall(function()
+          return not vim.tbl_contains({ "markdown", "text", "gitcommit", "oil" }, vim.bo.filetype)
+        end)
+        return ok and result
+      end,
+    },
+  },
 
   {
     "nvim-treesitter/nvim-treesitter",
@@ -59,6 +70,50 @@ return {
       opts = function()
         return require "configs.oil-nvim"
       end,
+    },
+  },
+
+  {
+    "wakatime/vim-wakatime",
+    event = "BufReadPost",
+  },
+
+  {
+    "catgoose/nvim-colorizer.lua",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = function()
+      return require "configs.nvim-colorizer"
+    end,
+  },
+
+  {
+    "nvim-pack/nvim-spectre",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>sr", "<cmd>Spectre<CR>", desc = "Spectre panel" },
+      {
+        "<leader>sw",
+        function()
+          require("spectre").open_visual { select_word = true }
+        end,
+        mode = "n", -- tambah mode eksplisit
+        desc = "Search word under cursor",
+      },
+      {
+        "<leader>sw",
+        function()
+          require("spectre").open_visual()
+        end,
+        mode = "v",
+        desc = "Search selection",
+      },
+      {
+        "<leader>sf",
+        function()
+          require("spectre").open_file_search { select_word = true }
+        end,
+        desc = "Search in current file",
+      },
     },
   },
 }
